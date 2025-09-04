18e9ddcf96b68fc432ed28b661d8df5e32d5d470
RayAnthoney <rmaxey911@gmail.com>
2025-09-04 11:06:32 -0600

    Setting up about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,30 @@
+import gsap from 'gsap';
+import { SplitText } from 'gsap/all'
 import {useGSAP} from "@gsap/react";
 
 const About = () => {
     useGSAP(() => {
-    const titleSplit = splitText.create( )
+        const titleSplit = SplitText.create( '#about h2', {
+            type: 'words'
+        })
+
+        const scrollTimeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: '#about',
+                start: 'top center'
+            }
+        })
+
+        scrollTimeline
+            .from(titleSplit.words, {
+                opacity: 0, duration: 1, yPercent: 100, ease: 'expo.out', stagger: 0.02
+            })
+            .from( '.top-grid div, .bottom-grid div', {
+            opacity: 0, duration: 1, ease: 'power1.inOut', stagger: 0.04
+        }, '-=0.5')
+
     })
+
 
     return (
         <div id="about">
